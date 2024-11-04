6ec423ff89fcf793fd4db72d26ac5c15d175bb21
markcirineo <markcirineo22@gmail.com>
2024-11-04 00:46:51 -0500

    Add stores path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import { resolve } from "path";
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@components": resolve(__dirname, "./src/components")
-    }
-  }
-})
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@components": resolve(__dirname, "./src/components"),
+            "@stores": resolve(__dirname, "./src/stores"),
+        },
+    },
+});
