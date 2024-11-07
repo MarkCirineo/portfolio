facf59eba85efe6059c1ec54227a96d6b8b3dbc4
markcirineo <markcirineo22@gmail.com>
2024-11-06 23:25:30 -0500

    Create Home.vue

---
+++ src/components/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home">
+        <Hero />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Hero from "@components/hero/Hero.vue";
+
+export default defineComponent({
+    name: "Home",
+
+    components: {
+        Hero,
+    },
+});
+</script>
+
+<style scoped>
+.home {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    height: 100%;
+}
+</style>
