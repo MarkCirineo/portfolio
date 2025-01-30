1a83c69c0f2ac8059230d955c8d52f1210c05d38
markcirineo <markcirineo22@gmail.com>
2025-01-29 23:57:35 -0500

    Add work section to Home page

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,28 +1,27 @@
 <template>
     <div class="home">
         <Hero />
+        <Work />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Hero from "@components/hero/Hero.vue";
+import Work from "@components/work/Work.vue";
 
 export default defineComponent({
     name: "Home",
 
     components: {
         Hero,
+        Work,
     },
 });
 </script>
 
 <style scoped>
 .home {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
     height: 100%;
 }
 </style>
