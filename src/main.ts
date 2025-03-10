a8837435d1b8b90c4b8c43353bb6620fdbc16535
markcirineo <markcirineo22@gmail.com>
2025-03-10 01:10:07 -0400

    Add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import FloatingVue from "floating-vue";
 import "floating-vue/dist/style.css";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).use(FloatingVue).mount("#app");
+createApp(App).use(router).use(createPinia()).use(FloatingVue).mount("#app");
